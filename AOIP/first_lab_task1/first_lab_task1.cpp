dffd09c7a1ee150c2da2ee3d4ab4f5ecce61fdd3
Leonenko Maksim <maxleon1611@gmail.com>
2023-11-07 16:37:13 +0300

    first lab task 1

---
+++ AOIP/first_lab_task1/first_lab_task1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+	double x, a, b, c, d, z1, z2;
+	cout << "введите значение x:";
+	cin >> x;
+	a = 1 - 2 * pow(sin(x), 2);
+	b = 1 + sin(2 * x);
+	c = 1 - tan(x);
+	d = 1 + tan(x);
+	z1 = a / b;
+	z2 = c / d;
+	cout << "значение z1: " << z1 << endl;
+	cout << "значение z2:" << z2 << endl;
+
+}
